7ac2b39a3e7b29c057a82b3b828725ef07053fec
Evgenyi <makarovee98@gmail.com>
2020-08-17 14:52:22 +0700

    fonts + forms

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*let dropDown = document.querySelector('.drop_down__icon');
+let dropDownBlock = document.querySelector('.drop_down__list');
+dropDown.onclick = function(){
+    //dropDownBlock.classList.toggle('hide');
+    $(".drop_down__list").toggleClass("hide");
+    console.log('work');
+}
+*/
